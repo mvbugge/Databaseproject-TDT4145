5e47c4bfa96e6fc2262512ca32b4bd34b2a7cb1b
Mai Ve Bugge <maivb@stud.ntnu.no>
2019-03-19 16:40:20 +0100

    send query to db

--- src/InputQueries.java
+++ src/InputQueries.java
@@ -1,45 +1,54 @@
 package treningsbok;
 
+import treningsbok.DBManager;
+
 public class InputQueries {
+	private static DBManager dbmanager;
+	
+	public InputQueries() {
+		dbmanager = new DBManager();
+		dbmanager.connect();
+		};
+		
 	public static void RegistrerApparat(String navn, String beskrivelse) {
 		String query = "insert into apparat (ApparatID, Navn, Beskrivelse) values("
 				+ "NULL, '" + navn + "', '" + beskrivelse + "');";
-		//TODO: Pass to DB
+		dbmanager.sendDB(query);
 	}
 	public static void RegistrerFriovelse(String navn, String beskrivelse) {
 		String query = "insert into ovelse (OvelseID, Navn) values("
 				+ "NULL, '" + navn + "', '" + beskrivelse + "');";
-		//TODO: Pass to DB
+		dbmanager.sendDB(query);
 	}
 	public static void RegistrerApparatovelse(String navn, String kilo, String sett, String apparatID) {
 		String query = "insert into ovelse (OvelseID, Navn, AntallKilo, AntallSett, ApparatID)"
 				+ " values(" + "NULL, '" + navn + "', '" + kilo + "', '" + sett + "', '"
 				+ apparatID + "'');";
-		//TODO: Pass to DB
+		dbmanager.sendDB(query);
 	}
 	public static void RegistrerOkt(String dato, String tidspunkt, String varighet,
 			String prestasjon, String form) {
 		String query = "insert into okt (OktID, Dato, Tidspunkt, Varighet, Prestasjon, Form) "
 				+ "values(" + "NULL, '" + dato + "', '" + tidspunkt + "', '" + varighet + "', '" +
 				prestasjon + "', '" + form + "');";
-		//TODO: Pass to DB
+		dbmanager.sendDB(query);
 	}
 	public static void RegistrerNotat(String beskrivelse, String oktID) {
 		String query = "insert into notat (NotatID, Beskrivelse, OktID) values(" + "NULL, '" 
 				+ beskrivelse + "', '" + oktID + "');";
-		//TODO: Pass to DB
+		dbmanager.sendDB(query);
 	}
 	public static void RegistrerGruppe(String navn) {
 		String query = "insert into ovelsesgruppe(GruppeID, Navn) values(" 
 				+ "NULL, '" + navn + "');";
-		//TODO: Pass to DB
+		dbmanager.sendDB(query);
 	}
 	public static void RegistrerOvelseIOkt(String ovelseID, String oktID) {
 		String query = "insert into ovelseiokt values('" + ovelseID + "', '" + oktID + "');";
-		//TODO: Pass to DB
+		dbmanager.sendDB(query);
 	}
 	public static void RegistrerInngarI(String ovelseID, String gruppeID) {
 		String query = "insert into inngari values('" + ovelseID + "', '" + gruppeID + "');";
-		//TODO: Pass to DB
+		dbmanager.sendDB(query);
 	}
 }
